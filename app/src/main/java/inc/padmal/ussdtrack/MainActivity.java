c2ec13d647983624b8940eb70010b5ac1a12cfd2
Padmal <blog.padmal@gmail.com>
2018-10-22 10:43:51 +0530

    feat: plots at startup

--- app/src/main/java/inc/padmal/ussdtrack/MainActivity.java
+++ app/src/main/java/inc/padmal/ussdtrack/MainActivity.java
@@ -14,28 +14,21 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     LineChart chartMoney, chartData;
     String ussd = "*100" + Uri.encode("#");
+    Plotter plotter;
+    SharedPreferences Logs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         chartMoney = findViewById(R.id.chartMoney);
         chartData = findViewById(R.id.chartData);
 
+        plotter = Plotter.getInstance();
+
+        Logs = getSharedPreferences("USSD Tracker", MODE_PRIVATE);
+
         startService(new Intent(getApplicationContext(), USSDService.class));
 
         LocalBroadcastManager.getInstance(this).registerReceiver(USSDReceiver,
@@ -55,57 +52,15 @@ public class MainActivity extends AppCompatActivity {
         SMSReceiver.bindListener(new SmsListener() {
             @Override
             public void messageReceived(String messageText) {
-                SharedPreferences prefs = getSharedPreferences("USSD Tracker", MODE_PRIVATE);
-                String DATAdata = prefs.getString("DATA", "");
-                try {
-                    JSONObject DATAJson = new JSONObject(DATAdata);
-                    DATAJson.put(String.valueOf(System.currentTimeMillis()), messageText);
-                    String updateJSONString = DATAJson.toString();
-                    SharedPreferences.Editor editor = getSharedPreferences("USSD Tracker", MODE_PRIVATE).edit();
-                    editor.putString("DATA", updateJSONString);
-                    editor.apply();
-                    updateDATAChart(DATAJson);
-                } catch (JSONException e) {
-                    JSONObject FirstJSON = new JSONObject();
-                    try {
-                        FirstJSON.put(String.valueOf(System.currentTimeMillis()), messageText);
-                        String updateJSONString = FirstJSON.toString();
-                        SharedPreferences.Editor editor = getSharedPreferences("USSD Tracker", MODE_PRIVATE).edit();
-                        editor.putString("DATA", updateJSONString);
-                        editor.apply();
-                        updateDATAChart(FirstJSON);
-                    } catch (JSONException j) {
-                        Toast.makeText(getApplicationContext(), "Error in parsing message", Toast.LENGTH_LONG).show();
-                    }
-                }
-                Log.d("Padmals", messageText);
+                processData(Logs.getString("DATA", ""),
+                        messageText, chartData, "DATA", "Data", Color.RED);
             }
         });
-    }
 
-    private void updateDATAChart(JSONObject i) {
-        List<Entry> entries = new ArrayList<>();
-        Iterator<String> iter = i.keys();
-        while (iter.hasNext()) {
-            String key = iter.next();
-            try {
-                String Data = i.getString(key);
-                entries.add(new Entry(Float.valueOf(key), Float.valueOf(Data)));
-            } catch (JSONException e) {
-                Toast.makeText(getApplicationContext(), "Error in rendering message", Toast.LENGTH_LONG).show();
-            }
-        }
-        chartData.getDescription().setEnabled(false);
-        HourAxisValueFormatter axisFormatter = new HourAxisValueFormatter(0);
-        chartData.getXAxis().setValueFormatter(axisFormatter);
-        LineDataSet dataSet = new LineDataSet(entries, "Data");
-        dataSet.setColor(Color.RED);
-        dataSet.setValueTextColor(Color.BLACK);
-        dataSet.setCircleColor(Color.BLACK);
-        dataSet.setCircleRadius(1);
-        LineData lineData = new LineData(dataSet);
-        chartData.setData(lineData);
-        chartData.invalidate();
+        plotter.loadCharts(getApplicationContext(), chartMoney,
+                Logs.getString("MONEY", ""), Color.BLUE, "Money");
+        plotter.loadCharts(getApplicationContext(), chartData,
+                Logs.getString("DATA", ""), Color.RED, "Data");
     }
 
     @Override
@@ -116,22 +71,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_review) {
-            if (ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + ussd)));
-            return true;
-        } else if (id == R.id.action_clean) {
-            SharedPreferences.Editor editor = getSharedPreferences("USSD Tracker", MODE_PRIVATE).edit();
-            editor.clear();
-            editor.apply();
-            chartMoney.invalidate();
-            chartMoney.clear();
-            chartData.invalidate();
-            chartData.clear();
+        switch (item.getItemId()) {
+            case R.id.action_review:
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + ussd)));
+                break;
+            case R.id.action_clean:
+                Logs.edit().clear().apply();
+                chartMoney.invalidate();
+                chartMoney.clear();
+                chartData.invalidate();
+                chartData.clear();
+                break;
+            default:
+                break;
         }
         super.onOptionsItemSelected(item);
         return true;
@@ -141,58 +97,30 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             String message = intent.getStringExtra("USSD");
-            String amount = message.substring(message.indexOf("Rs.") + 4, message.indexOf("will") - 1);
-
-            SharedPreferences prefs = getSharedPreferences("USSD Tracker", MODE_PRIVATE);
-            String MONEYdata = prefs.getString("MONEY", "");
-            try {
-                JSONObject MONEYJson = new JSONObject(MONEYdata);
-                MONEYJson.put(String.valueOf(System.currentTimeMillis()), amount);
-                String updateJSONString = MONEYJson.toString();
-                SharedPreferences.Editor editor = getSharedPreferences("USSD Tracker", MODE_PRIVATE).edit();
-                editor.putString("MONEY", updateJSONString);
-                editor.apply();
-                updateMONEYChart(MONEYJson);
-            } catch (JSONException e) {
-                JSONObject FirstJSON = new JSONObject();
-                try {
-                    FirstJSON.put(String.valueOf(System.currentTimeMillis()), amount);
-                    String updateJSONString = FirstJSON.toString();
-                    SharedPreferences.Editor editor = getSharedPreferences("USSD Tracker", MODE_PRIVATE).edit();
-                    editor.putString("MONEY", updateJSONString);
-                    editor.apply();
-                    updateMONEYChart(FirstJSON);
-                } catch (JSONException j) {
-                    Toast.makeText(getApplicationContext(), "Error in parsing money", Toast.LENGTH_LONG).show();
-                }
-            }
-            Log.d("Padmals", amount);
+            processData(Logs.getString("MONEY", ""),
+                    message.substring(message.indexOf("Rs.") + 4, message.indexOf("will") - 1),
+                    chartMoney, "MONEY", "Money", Color.BLUE);
         }
     };
 
-    private void updateMONEYChart(JSONObject i) {
-        List<Entry> entries = new ArrayList<>();
-        Iterator<String> iter = i.keys();
-        while (iter.hasNext()) {
-            String key = iter.next();
+    private void processData(String JStr, String msg, LineChart chart, String Key, String label, int color) {
+        try {
+            JSONObject jsonObject = new JSONObject(JStr);
+            jsonObject.put(String.valueOf(System.currentTimeMillis()), msg);
+            String updateJSONString = jsonObject.toString();
+            Logs.edit().putString(Key, updateJSONString).apply();
+            plotter.plotChart(getApplicationContext(), jsonObject, chart, label, color);
+        } catch (JSONException e) {
+            JSONObject FirstJSON = new JSONObject();
             try {
-                String Data = i.getString(key);
-                entries.add(new Entry(Float.valueOf(key), Float.valueOf(Data)));
-            } catch (JSONException e) {
-                Toast.makeText(getApplicationContext(), "Error in rendering money", Toast.LENGTH_LONG).show();
+                FirstJSON.put(String.valueOf(System.currentTimeMillis()), msg);
+                String updateJSONString = FirstJSON.toString();
+                Logs.edit().putString(Key, updateJSONString).apply();
+                plotter.plotChart(getApplicationContext(), FirstJSON, chart, label, color);
+            } catch (JSONException j) {
+                Toast.makeText(getApplicationContext(), "Error in parsing", Toast.LENGTH_LONG).show();
             }
         }
-        chartMoney.getDescription().setEnabled(false);
-        HourAxisValueFormatter axisFormatter = new HourAxisValueFormatter(0);
-        chartMoney.getXAxis().setValueFormatter(axisFormatter);
-        LineDataSet dataSet = new LineDataSet(entries, "Money");
-        dataSet.setColor(Color.BLUE);
-        dataSet.setCircleColor(Color.BLACK);
-        dataSet.setCircleRadius(1);
-        dataSet.setValueTextColor(Color.BLACK);
-        LineData lineData = new LineData(dataSet);
-        chartMoney.setData(lineData);
-        chartMoney.invalidate();
     }
 
     @Override
